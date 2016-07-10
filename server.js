ac2d006f24f374a3f83a644c2240673d04c1a22b
jackywian <wianjacky@yahoo.com>
2016-07-10 22:30:39 +0700

    final

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 global.__root = __dirname + '/';
-var express = require('express'),
-	router = require('./router')
-	app = express();
+
+var http = require('http');
+var express = require('express');
+var router = require('./router');
+var app = express();
 
 app.use("/", express.static(__dirname + "/view"));
 router(app);
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function(){
-	console.log('Listening at ' + app.get('port'));
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function() {
+    console.log('Listening at ' + app.get('port'));
 });
